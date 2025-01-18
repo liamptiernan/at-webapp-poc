2dc1acc6d33ead3114f8f9bd833070a878ff886e
Liam Tiernan <liamptiernan@gmail.com>
2025-01-17 23:19:52 -0500

    initial airtable setup

--- functions/main.py
+++ functions/main.py
@@ -2,12 +2,27 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
+from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
+from pyairtable import Api
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+from settings import settings
+
+initialize_app()
+
+def create_airtable_client(api_key: str) -> Api:
+    return Api(api_key)
+
+
+@https_fn.on_request(
+        secrets=[settings.airtable_base_id, settings.airtable_api_key],        
+        cors=options.CorsOptions(
+        cors_origins=["http://localhost:3000"],
+        cors_methods=["get", "post"],
+    ))
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    print("yes")
+    airtable = create_airtable_client(settings.airtable_api_key.value)
+    table = airtable.table(settings.airtable_base_id.value, "Projects")
+
+    return {"data": {"records": table.all()}}
